9c54bd4a570b273e1e7b938f4cc95d4f8ba00b9d
Ivan <hladysh.im@gmail.com>
2016-12-16 11:04:16 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+  <meta charset="utf-8"/>
+</head>
+
+<body>
+  <form action="submit.php" method="POST">
+    <input name="email" placeholder="Email"><br>
+    <input name="name" placeholder="Name"><br>
+    <input name="phone" placeholder="Phone"><br>
+    <textarea name="message" placeholder="Messeges"></textarea><br>
+    <input type="submit" value="Send messeges">
+  </form>
+</body>
+</html>
